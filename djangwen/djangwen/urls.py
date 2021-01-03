d0d137806cd09c13c3ee62a33675bea4ffea91ca
Aleks <alekkswithak@gmail.com>
2021-01-03 20:22:54 +0000

    Basic deck display

--- djangwen/djangwen/urls.py
+++ djangwen/djangwen/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from flashcards import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.DeckListView.as_view(), name='deck-list'),
 ]
--- djangwen/flashcards/views.py
+++ djangwen/flashcards/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from flashcards.models import (
+    Deck
+)
+from django.views.generic.list import ListView
 
-# Create your views here.
+
+class DeckListView(ListView):
+    model = Deck
+    template_name = 'deck_list.html'
+    paginate_by = 12
